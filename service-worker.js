0549931b21821db2d6dafe61cc7a907b1f952258
Serhii Alieksieievych <kajot1863@gmail.com>
2021-01-18 00:25:19 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.3f38e2a5b3c1860d903e30d6bfc2369e.js"
 );
 
 self.addEventListener('message', (event) => {
